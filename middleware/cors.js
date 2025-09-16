b9e0e25a9a84eaddafab3b6bae24b521c5d93217
Travis Reeder <treeder@gmail.com>
2025-09-16 13:26:34 -0600

    Refactor CORS middleware to handle OPTIONS requests

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,7 +1,16 @@
 export async function cors(c) {
-  const response = await c.next()
-  response.headers.set('Access-Control-Allow-Origin', '*')
-  response.headers.set('Access-Control-Max-Age', '86400')
-  response.headers.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE')
-  return response
+  let r
+  if(c.request.method == 'OPTIONS'){
+    r = new Response(null, {
+      status: 204,
+    })
+  } else {
+    r = await c.next()
+  }
+  r.headers.set('Access-Control-Allow-Origin', '*')
+  r.headers.set('Access-Control-Max-Age', '86400')
+  // response.headers.set('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS')
+  r.headers.set('Access-Control-Allow-Methods', '*')
+  r.headers.set('Access-Control-Allow-Headers', '*')
+  return r
 }
