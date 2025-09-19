92a6bc25a0379a70c17e85e61c1fdc886f459c50
treeder <treeder@gmail.com>
2025-09-19 21:33:08 +0000

    use console.log

--- utils.js
+++ utils.js
@@ -1,10 +1,10 @@
 export async function awaitAll(c, promises) {
   let results = await Promise.allSettled(promises)
-  c.data.logger.log('awaitAll results:', results)
+  // c.data.logger.log('awaitAll results:', results)
   for (let r of results) {
     // c.data.logger.log(r)
     if (r.status == 'rejected') {
-      c.data.logger.log(`awaitAll error:`, r.reason)
+      console.log(`awaitAll error:`, r.reason)
       throw r.reason
     }
   }
