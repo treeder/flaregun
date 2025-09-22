21a3d0ccb4068b19c5b4b8dc88c10f907713a398
treeder <treeder@gmail.com>
2025-09-22 22:15:40 +0000

    export logger in index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { D1 } from './d1.js'
 import { Scheduler } from './scheduler.js'
 import { ErrorHandler } from './errors.js'
+import { CloudflareLogger } from './logger.js'
 
-export { D1, Scheduler, ErrorHandler }
+export { D1, Scheduler, ErrorHandler, CloudflareLogger }
